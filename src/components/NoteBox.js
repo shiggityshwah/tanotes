bdcbf2d6e15a5e7ec9b252ae17aa4893d34f1b58
Stefano M <stefanom@gmail.com>
2019-02-14 01:04:21 -0800

    initial project with material-ui

---
+++ src/components/NoteBox.js
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+    Paper,
+    Grid,
+    TextField,
+    Button,
+    Typography
+} from "@material-ui/core";
+import "../containers/App.css";
+import "typeface-pacifico";
+
+const NoteBox = ({
+    currentNoteTitle,
+    currentNoteText,
+    titleChange,
+    textChange
+}) => {
+    return (
+        <Grid container justify="flex-end" alignItems="center" style={{ padding: "50px" }}>
+            <Grid item xs={12}>
+                <Grid container alignItems="center">
+                    <Grid item>
+                        <Typography
+                            variant="h2"
+                            style={{ fontFamily: "'Pacifico', cursive" }}
+                        >
+                            Title:
+                        </Typography>
+                    </Grid>
+                    <Grid item md={true} sm={12} xs={12}>
+                        <Paper
+                            style={{
+                                margin: "30px",
+                                padding: "6px 20px 10px 20px",
+                                backgroundColor: "#212121"
+                            }}
+                        >
+                            <TextField
+                                fullWidth
+                                id="title"
+                                value={currentNoteTitle}
+                                onChange={titleChange}
+                                margin="normal"
+                                variant="outlined"
+                            />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container alignItems="flexStart">
+                    <Grid item>
+                        <Typography
+                            variant="h2"
+                            style={{ fontFamily: "'Pacifico', cursive" }}
+                        >
+                            Note:
+                        </Typography>
+                    </Grid>
+                    <Grid item md={true} sm={12} xs={12}>
+                        <Paper
+                            style={{
+                                margin: "30px",
+                                padding: "6px 20px 10px 20px",
+                                backgroundColor: "#212121"
+                            }}
+                        >
+                            <TextField
+                                fullWidth
+                                id="text"
+                                multiline
+                                rows="4"
+                                defaultValue="Default Value"
+                                margin="normal"
+                                variant="outlined"
+                                value={currentNoteText}
+                                onChange={textChange}
+                            />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item>
+                <Button
+                    
+                    variant="raised" 
+                    color="primary" 
+                    style={{ textTransform: "none", margin:"0px 50px"}}
+                >
+                    SAVE NOTE
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+export default NoteBox;
---
+++ src/containers/App.js
@@ -0,0 +1,178 @@
+import React, { Component } from "react";
+import "./App.css";
+import NoteList from "../components/NoteList";
+import NoteBox from "../components/NoteBox";
+import NavBar from "../components/NavBar";
+import Register from "../components/Register";
+import Grid from "@material-ui/core/Grid";
+import CheckBox from "@material-ui/icons/CheckBox";
+import CheckBoxOutlineBlank from "@material-ui/icons/CheckBoxOutlineBlank";
+import "./App.css";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import yellow from "@material-ui/core/colors/yellow";
+
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: yellow,
+        secondary: {
+            main: "#f44336"
+        }
+    }
+});
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            currentNoteTitle: "my note",
+            currentNoteText: "hi, this is my note",
+            isSaved: false,
+            currentNote: 0,
+            noteTitles: ["test1", "test2"],
+            noteTexts: ["test1", "test2"],
+            signedIn: true
+        };
+
+        this.timeoutId = null;
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onTextChange = this.onTextChange.bind(this);
+        this.onCreateNote = this.onCreateNote.bind(this);
+        this.onClickTitle = this.onClickTitle.bind(this);
+        this.saveToDatabase = this.saveToDatabase.bind(this);
+    }
+    /* 
+  componentDidMount() {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(db => {
+        if (db.noteTexts) {
+            console.log(db.noteTexts)
+        }
+      })
+      .catch('');
+  }
+ */
+    onTitleChange = event => {
+        const newTitles = this.state.noteTitles.slice();
+        newTitles[this.state.currentNote] = event.target.value;
+        this.setState({ noteTitles: newTitles });
+        this.saveToDatabase();
+    };
+
+    onTextChange = event => {
+        const newTexts = this.state.noteTexts.slice();
+        newTexts[this.state.currentNote] = event.target.value;
+        this.setState({ noteTexts: newTexts });
+        this.saveToDatabase();
+    };
+
+    onCreateNote = () => {
+        const newNote = this.state.noteTitles.length;
+        const newTitles = [...this.state.noteTitles, ""];
+        const newTexts = [...this.state.noteTexts, ""];
+        this.setState({ noteTitles: newTitles });
+        this.setState({ noteTexts: newTexts });
+        this.setState({ currentNote: newNote });
+    };
+
+    onClickTitle = event => {
+        const title = event.target.id;
+        this.setState({ currentNote: title });
+    };
+
+    saveToDatabase() {
+        this.setState({ isSaved: false });
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+        }
+        this.timeoutId = setTimeout(() => {
+            this.setState({ isSaved: true });
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <Grid
+                    container
+                    spacing={12}
+                    alignItems="stretch"
+                    style={{ backgroundColor: "#424242" }}
+                >
+                    <Grid item xs={12}>
+                        <NavBar />
+                    </Grid>
+                    <Grid item xs={12}>
+                        {this.state.signedIn === false ? (
+                            <Register />
+                        ) : (
+                            <Grid container alignItems="stretch" spacing={12}>
+                                <Grid item xs={12} sm={12} md={9}>
+                                    <NoteBox
+                                        currentNoteTitle={
+                                            this.state.noteTitles[
+                                                this.state.currentNote
+                                            ]
+                                        }
+                                        currentNoteText={
+                                            this.state.noteTexts[
+                                                this.state.currentNote
+                                            ]
+                                        }
+                                        titleChange={this.onTitleChange}
+                                        textChange={this.onTextChange}
+                                    />
+                                </Grid>
+                                <Grid item alignItems="stretch" xs={12} sm={12} md={3}>
+                                    <NoteList
+                                        noteTitles={this.state.noteTitles}
+                                        clickTitle={this.onClickTitle}
+                                        createNote={this.onCreateNote}
+                                    />
+                                </Grid>
+                            </Grid>
+                        )}
+                    </Grid>
+                    <Grid item xs={12}>
+                        <div className="footer">
+                            <Grid container spacing={16}>
+                                <Grid
+                                    item
+                                    xs={12}
+                                >
+                                    <Grid container justify="center" alignItems="center">
+                                        <Grid item>
+                                          Saved:
+                                        </Grid>
+                                        <Grid item>
+                                            {this.state.isSaved ? (
+                                                <CheckBox
+                                                    style={{ fontSize: 40 }}
+                                                />
+                                            ) : (
+                                                <CheckBoxOutlineBlank
+                                                    style={{ fontSize: 40 }}
+                                                />
+                                            )}
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12} style={{ textAlign: "center" }} >
+                                    Current Note:{" "}
+                                    {
+                                        this.state.noteTitles[
+                                            this.state.currentNote
+                                        ]
+                                    }
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Grid>
+                </Grid>
+            </MuiThemeProvider>
+        );
+    }
+}
+
+export default App;
